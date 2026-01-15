efcb2563487cadf2354264d89611e5b0275ea551
Horatiu23 <horatiublaj1001@gmail.com>
2026-01-15 13:28:56 +0200

    Fixed smth."

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import Dashboard from "./components/Dashboard";
 import DoctorAppointments from "./components/DoctorAppts";
 import PatientAppointments from "./components/PatientAppts";
 import { UserProfile } from "./components/UserProfile";
-import PatientAppointments from "./components/PatientAppts";
-import DoctorAppointments from "./components/DoctorAppts";
 
 export interface User {
   id: number;
